1ea3c495e291a542709cd861ff0ca20a1d52be33
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-05-25 15:38:49 +0530

    Add files via upload

---
+++ CatImage.java
@@ -0,0 +1,14 @@
+public class CatImage
+{
+  public static void main(String[] args)
+ {
+   System.out.println("   /\\        /\\  ");
+   System.out.println("  /  \\______/  \\ ");
+   System.out.println(" /              \\ ");
+   System.out.println("(  /\\        /\\   )");
+   System.out.println("=====    V    =====");
+   System.out.println("=======(_|_)=======");
+   System.out.println(" (               ) ");
+   System.out.println("  (_____________)  ");
+}
+}
