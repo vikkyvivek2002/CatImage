b8ac5a832593898d60f00db5c57645873a862387
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-05-25 15:39:54 +0530

    Update CatImage.java

--- CatImage.java
+++ CatImage.java
@@ -2,13 +2,13 @@ public class CatImage
 {
   public static void main(String[] args)
  {
-   System.out.println("   /\\        /\\  ");
-   System.out.println("  /  \\______/  \\ ");
-   System.out.println(" /              \\ ");
-   System.out.println("(  /\\        /\\   )");
-   System.out.println("=====    V    =====");
-   System.out.println("=======(_|_)=======");
-   System.out.println(" (               ) ");
-   System.out.println("  (_____________)  ");
+   System.out.println("   /\\        /\\   ");
+   System.out.println("  /  \\______/  \\  ");
+   System.out.println(" /               \\ ");
+   System.out.println("(  /\\       /\\  ) ");
+   System.out.println("=====    V    ===== ");
+   System.out.println("=======(_|_)======= ");
+   System.out.println(" (               )  ");
+   System.out.println("  (_____________)   ");
 }
 }
